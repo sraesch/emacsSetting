50e7a55f43727e29c88840e5f7018f19769d2763
Sascha Raesch <Sascha.Raesch@gmail.com>
2016-02-02 21:29:51 +0100

    - First initial themes - Changed font

---
+++ init.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("f23f0dc478c8c33a56f65365afc976d8eb2ed48391f43a659ad4f6f86c97b397" "2da65cb7074c176ca0a33f06bcc83ef692c9175e41b6370f5e94eb5811d6ee3a" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;(set-face-attribute 'default nil :height 180)
+(set-face-attribute 'default t :font  "Source Code Pro Semibold" :height 200)
